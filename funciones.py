60300082cf92de6df1cb1c0a3b9b6de0ff7607bd
Ivan Ruiperez <ivanruiperez.instituto@gmail.com>
2024-04-28 19:42:25 +0200

    Inserción simple

--- funciones.py
+++ funciones.py
@@ -1,13 +1,14 @@
 #Iván Ruipérez Benítez
-import pymongo
-
+from pymongo import MongoClient
+import sys
+import pprint
 def menu():
     print()
     print('''Menú
     1. Insertar receta.
     2. Borrar receta.
     3. Modificar nombre de receta.
-    4. Mostrar nombre de las recetas.
+    4. Mostrar el nombre de las recetas con más de 3 comensales.
     5. Mostrar recetas con etiqueta Pollo desmenuzado.
     6. Mostrar los ingredientes de la receta Ensalada César.
     7. Mostrar el número de recetas de cada autor.
@@ -18,3 +19,45 @@ def menu():
         print("Esa opción no existe.")
         num=input("Elija una opción: ")
     return int(num)
+
+def conexionMongo():
+    try:
+        client = MongoClient('localhost')
+        db = client.Cocina
+        col = db.recetas
+    except :
+        print("No hay conexión con Mongo.")
+        sys.exit(1)    
+    return col
+
+def opciones(num,db):
+    if int(num) == 1:
+        Insertar(db)
+    elif int(num) == 2:
+        Eliminar(db)
+    elif int(num) == 3:
+        Modificar(db)
+    elif int(num) == 4:
+        NombreRecetas(db)
+    elif int(num) == 5:
+        PolloDesmenuzado(db)
+    elif int(num) == 6:
+        IngEnsaladaCesar(db)
+    elif int(num) == 7:
+        ContRecetasAutor(db)
+        
+def Insertar(db):
+    print('1')
+
+def Eliminar(db):
+    print('2')
+    
+def Modificar(db):
+    print('3')
+
+def NombreRecetas(db):
+    doc={"servings": {"$gt": 3} }
+    resultado=db.find(doc, {"_id":0,"name":1})
+    for documento in resultado:
+        name=documento.get("name")
+        print(name)
--- progama.py
+++ progama.py
@@ -1,1 +1,8 @@
 #Iván Ruipérez Benítez
+from funciones import conexionMongo,menu,opciones
+
+db=conexionMongo()
+num=0
+while num != 8:
+    num=menu()
+    opciones(num,db)
